b910e7c3f9da9bcef84eff3851fa8fee1aa08ca1
Muhammad Ali Mahmood <ali408mehmood@gmail.com>
2023-12-08 22:39:18 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Ali Mahmood",
+  title: "Flutter Developer and Data Analyst",
+  email: "ali408mehmood@gmail.com",
+  gitHub: "ali4008",
+  instagram: "alimehmood408",
+  linkedIn: "Muhammad Ali Mahmood",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
